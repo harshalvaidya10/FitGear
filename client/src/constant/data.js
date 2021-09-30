3f88451c8262a0d3513027e6f89ba58f5aeeb330
harshalvaidya10 <vaidyahome.hv@gmail.com>
2021-09-30 20:01:44 +0530

    First commit

---
+++ client/src/constant/data.js
@@ -0,0 +1,134 @@
+export const navData = [
+    { url: 'https://rukminim1.flixcart.com/flap/128/128/image/f15c02bfeb02d15d.png?q=100', text: 'Top Offers' },
+    { url: 'https://rukminim1.flixcart.com/image/416/416/k9zyqvk0/treadmill/h/n/c/td-m1-motorized-powermax-fitness-original-imafrz9fmddqjgrh.jpeg?q=70', text: 'Treadmill' },
+    { url: 'https://rukminim1.flixcart.com/image/416/416/kqpj4i80/cross-trainer/l/u/1/wc6066-elliptical-with-lcd-display-adjustable-seat-adjustable-original-imag4nkq5qerh2xu.jpeg?q=70', text: 'Elliptical' },
+    { url: 'https://cdn.shopify.com/s/files/1/0046/8752/8024/products/DBHR550SET__54861_d130e685-079a-480d-905c-2ad899f042ee_1200x.jpg?v=1619197139', text: 'Dumbbells' },
+    { url: 'https://cdn.shopify.com/s/files/1/0046/8752/8024/products/UBK_665_Black_Pearl__68280.1558321873_aad789fa-9dbc-48ec-b16c-0678e0c6dcdc_1200x.jpg?v=1631910145', text: 'Exercise Bikes' },
+    { url: 'https://cdn.shopify.com/s/files/1/0046/8752/8024/products/25LB-0920840_1200x.jpg?v=1625516289', text: 'Exercise Plates' },
+    { url: 'https://rukminim1.flixcart.com/image/416/416/ks0onm80/protein-supplement/9/e/w/beginner-s-with-shaker-nut5903-01-muscleblaze-original-imag5zdrwghrzvvx.jpeg?q=70', text: 'Supplements' },
+    { url: 'https://thumbs.dreamstime.com/z/muscular-man-working-out-doing-exercises-dumbbells-biceps-strong-male-naked-torso-abs-isolated-over-white-background-67698224.jpg', text: 'Workouts' },
+    { url: 'https://www.fitness1440.com/wp-content/uploads/2017/12/portion-control-300x300.jpg', text: 'Nutrition Plan' }
+];
+
+export const bannerData = [
+    'https://www.bodylinesports.co.in/common/images/new-banner1.jpg?v=1',
+    'https://stfitnessequipments.com/wp-content/uploads/2020/06/fitness-accecories-shop-banner-trivandrum03.jpg',
+    'https://stfitnessequipments.com/wp-content/uploads/2020/06/fitness-accecories-shop-banner-trivandrum02.jpg',
+    'https://www.fitness-world.in/wp-content/uploads/2021/01/Home-Gym-Equipment-Must-Haves-to-Get-Fit-in-2021.jpg'
+]
+
+export const dealData = [
+    { 
+        id: 'product1',
+        url: 'https://rukminim1.flixcart.com/image/200/200/khf63680/cases-covers/back-cover/d/7/g/spigen-acs02256-original-imafxfgbffqaugur.jpeg?q=70', 
+        detailUrl: '',
+        title: {
+            shortTitle: 'Mobile Covers'
+        }, 
+        discount: 'Extra 10% Off', 
+        tagline: 'Deal of the day' 
+    },
+    { 
+        id: 'product2',
+        url: 'https://rukminim1.flixcart.com/image/200/200/k5lcvbk0/moisturizer-cream/9/w/g/600-body-lotion-aloe-hydration-for-normal-skin-nivea-lotion-original-imafz8jb3ftt8gf9.jpeg?q=70', 
+        title: {
+            shortTitle: 'Skin & Hair Care'
+        },
+        discount: 'From 99+5% Off', 
+        tagline: 'Shampoos, Face Washes & More' 
+    },
+    { 
+        id: 'product3',
+        url: 'https://rukminim1.flixcart.com/flap/200/200/image/74bc985c62f19245.jpeg?q=70', 
+        title: {
+            shortTitle: 'Skybags & Safari'
+        }, 
+        discount: 'Upto 70% Off', 
+        tagline: 'Deal of the Day' 
+    },
+    { 
+        id: 'product4',
+        url: 'https://rukminim1.flixcart.com/image/300/300/kll7bm80/smartwatch/c/1/n/43-mo-sw-sense-500-android-ios-molife-original-imagyzyycnpujyjh.jpeg?q=70', 
+        detailUrl: 'https://rukminim1.flixcart.com/image/416/416/kll7bm80/smartwatch/c/1/n/43-mo-sw-sense-500-android-ios-molife-original-imagyzyycnpujyjh.jpeg?q=70',
+        title: {
+            shortTitle: 'Smart Watches',
+            longTitle: 'Molife Sense 500 Smartwatch  (Black Strap, Freesize)',
+        }, 
+        price: {
+            mrp: 6999,
+            cost: 4049,
+            discount: '42%'
+        },
+        description: 'The Molife Sense 500, a brilliant smartwatch with a beautiful large display. Say hello to the infinity 1.7-inch display with 2.5D curved edges. Thanks to seamless Bluetooth 5.0 connectivity, you wont have to keep waiting. Bring a change to your outfit every day with changeable straps. A splash of color every day keeps the boredom away.',
+        discount: 'Grab Now', 
+        tagline: 'Best Seller' 
+    },
+    { 
+        id: 'product5',
+        url: 'https://rukminim1.flixcart.com/flap/150/150/image/b616a7aa607d3be0.jpg?q=70', 
+        title: {
+            shortTitle: 'Sports & Fitness Essentials'
+        }, 
+        discount: 'Upto 80% Off', 
+        tagline: 'Ab Exerciser, Yoga & more' 
+    }
+];
+
+export const furnitureData = [
+    { 
+        url: 'https://rukminim1.flixcart.com/image/300/300/ke7ff680/hammock-swing/j/f/u/q3-jkaf-y3l0-furniture-kart-original-imafux96kpy7grch.jpeg?q=70', 
+        title: {
+            shortTitle: 'Hammock And Swings'
+        }, 
+        discount: 'From ₹199', 
+        tagline: 'Trendy Collection' 
+    },
+    { 
+        url: 'https://rukminim1.flixcart.com/image/300/300/bean-bag/h/v/b/f8-the-furniture-store-xxxl-original-imae65d3wg7qzpkn.jpeg?q=70', 
+        title: {
+            shortTitle: 'Bean Bag Covers'
+        }, 
+        discount: 'Min 80% Off', 
+        tagline: 'XL, XXL & More' 
+    },
+    { 
+        url: 'https://rukminim1.flixcart.com/image/300/300/jlcmavk0/aquarium-tank/s/4/5/usb-desktop-aquarium-with-running-water-calendar-temperature-and-original-imaf8hv4nkv55gx8.jpeg?q=70', 
+        title: {
+            shortTitle: 'Aquarium Tank'
+        }, 
+        discount: 'From ₹299', 
+        tagline: 'Flat, Round, Cube & More' 
+    },
+    { 
+        url: 'https://rukminim1.flixcart.com/image/300/300/jffpoy80/office-study-chair/v/v/z/pp-am-5001cb-apex-original-imaf3u8rbr5cdycv.jpeg?q=70', 
+        title: {
+            shortTitle: 'Office & Study Chairs'
+        }, 
+        discount: 'Min 50% Off', 
+        tagline: 'Fabric & Leatherette' 
+    },
+    { 
+        url: 'https://rukminim1.flixcart.com/image/300/300/jvcp9jk0/recliner/z/w/x/brown-top-grain-leather-sf7018011-1-royaloak-original-imafg9s9hh9vzpf3.jpeg?q=70', 
+        title: {
+            shortTitle: 'Recliner'
+        }, 
+        discount: 'From ₹4999', 
+        tagline: 'Bantia, RoyalOak & More' 
+    },
+    { 
+        url: 'https://rukminim1.flixcart.com/image/300/300/jlqwpe80-1/tv-entertainment-unit/d/t/f/particle-board-holland-tv-unit-black-forzza-black-original-imaf8t5ybywcdtys.jpeg?q=70', 
+        title: {
+            shortTitle: 'Tv Units'
+        }, 
+        discount: 'From ₹2100', 
+        tagline: 'Forzza, Zuari & more' 
+    },
+    { 
+        url: 'https://rukminim1.flixcart.com/image/300/300/inflatable-sofa/6/j/s/wsb031a-velvet-wds-original-imaeaphzbkgrz3xp.jpeg?q=70', 
+        title: {
+            shortTitle: 'Inflatable Sofas'
+        }, 
+        discount: 'Min 50% Off', 
+        tagline: 'Furn Central & more' 
+    },
+];
