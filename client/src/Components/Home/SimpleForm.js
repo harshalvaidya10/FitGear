47459a79579863707757cee86d9f6f29917761dc
yashvivaghela <yashvivaghela@gmail.com>
2021-10-13 12:10:40 +0530

    Chatbot update

--- client/src/Components/Home/SimpleForm.js
+++ client/src/Components/Home/SimpleForm.js
@@ -19,7 +19,7 @@ const theme = {
   botFontColor: '#fff',
   userBubbleColor: '#fff',
   userFontColor: '#4a4a4a',
- 
+
 };
 
 
@@ -27,108 +27,196 @@ class SimpleForm extends Component {
   render() {
     return (
       <ThemeProvider theme={theme}>
-      <ChatBot
-      
-        steps={[
-          
+        <ChatBot
 
-          {
-            id: 'intro',
-            message: 'Hello! I am Jaadu. How may I help you?' ,
-            trigger: 'intro1'
-          },
+          steps={[
 
-          {
-            id: 'intro1',
-            message: 'Do you want to open BMI Calculator or want to know about different body types or exit?',
-            trigger: 'intro-user',
-          },
-          {
-            id: 'intro-user',
-            options: [
-              { value: 'y', label: 'BMI calculator', trigger: 'yes-response' },
-              { value: 'n', label: 'Different Body Types', trigger: 'no-response' },
-              { value: 'e', label: 'Exit', trigger: 'exit-response' },
-            ]
-          },
-          {
-            id: 'yes-response',
-            // message: 'Great!',
-            message: 'Enter you weight in kg',
-            trigger: 'weight',
-          },
-          {
-            id: 'weight',
-            user: true,
-            trigger:'yes-response1'
-          },
-          {
-            id: 'yes-response1',
-            // message: 'Great!',
-            message: 'Enter you height in cms',
+
+            {
+              id: 'intro',
+              message: 'Hello! I am Jaadu. How may I help you?',
+              trigger: 'intro1'
+            },
+
+            {
+              id: 'intro1',
+              message: 'Do you want to open BMI Calculator or want to know about the different body types?',
+              trigger: 'intro-user',
+            },
+            {
+              id: 'intro-user',
+              options: [
+                { value: 'y', label: 'BMI calculator', trigger: 'yes-response' },
+                { value: 'n', label: 'Different Body Types', trigger: 'no-response' },
+                { value: 'e', label: 'Exit', trigger: 'exit-response' },
+              ]
+            },
+            //BMI calculator
+            {
+              id: 'yes-response',
+              // message: 'Great!',
+              message: 'Enter you weight in kg',
+              trigger: 'weight',
+            },
+            {
+              id: 'weight',
+              user: true,
+              trigger: 'yes-response1'
+            },
+            {
+              id: 'yes-response1',
+              // message: 'Great!',
+              message: 'Enter you height in cms',
+
+              trigger: 'height',
+            },
+            {
+              id: 'height',
+              user: true,
+              trigger: 'calculator'
+            },
+
+            {
+              id: 'calculator',
+              // message: 'Great!',
+
+              component: <Post />,
+              asMessage: true,
+
+              trigger: 'intro1',
+            },
+
+
+
+            //Body-type
+            {
+              id: 'no-response',
+              message: 'Select',
+              trigger: 'body-types'
+            },
+            {
+              id: 'body-types',
+              options: [
+                { value: 'e', label: 'ECTOMORPH', trigger: '1' },
+                { value: 'm', label: 'MESOMORPH', trigger: '2' },
+                { value: 'endo', label: 'ENDOMORPH', trigger: '3' },
+              ]
+            },
+            //first body-type
+            {
+              id: '1',
+              message: 'The Ectomorph is naturally very thin, has narrow hips and shoulders, very low body fat and very thin arms and legs',
+              trigger: 'diet1',
+            },
+            {
+              id: 'diet1',
+              message: 'Do you want to see the diet plan for this particular body-type?',
+              trigger: 'yesno1',
+
+            },
+            {
+              id: 'yesno1',
+              options: [
+                { value: 'yy', label: 'Diet Plan ', trigger: 'yes1' },
+                { value: 'nn', label: 'Workout Plan', trigger: 'no1' },
+              ]
+            },
+            {
+              id: 'yes1',
+              message: `Ectomorphs should consume complex carbohydrates found in foods like beans, whole grains, and vegetables, 
+            rather than simple carbohydrates from highly processed foods, like potato chips and white bread.
+            `,
+            trigger:'intro1',
             
-            trigger: 'height',
-          },
-          {
-            id: 'height',
-            user: true,
-            trigger:'calculator'
-          },
-          {
-            id: 'calculator',
-            // message: 'Great!',
-            
-            component: <Post />,
-            asMessage: true,
-            
-            trigger: 'intro1',
-          },
+            },
+            {
+              id: 'no1',
+              message: ` For an ectomorph, cardio shouldn’t be the main emphasis of a workout. Experts suggest keeping aerobic training to a minimum and focusing on building muscle with activities like resistance training. 
+              This kind of physical activity allows ectomorphs to build mass but burn the fewest calories.
+            `,
+            trigger:'intro1',
+            },
           
-          
+            //second body type
+            {
+              id: '2',
+              message: 'The Mesomorph is kind of in between the Ectomorph and the Endomorph. They are able to put on muscle easily and genetically are the ideal body type for bodybuilding. They have very strong legs, broad shoulders and a narrower waist. Generally, they also have very low body fat as well.',
+              trigger: 'diet2',
+            },
+            {
+              id: 'diet2',
+              message: 'Do you want to see the diet plan or workout plan for this particular body-type?',
+              trigger: 'yesno2',
 
-//Body-type
-          {
-            id: 'no-response',
-            message: 'Select',
-            trigger: 'body-types'
-          },
-          {
-            id: 'body-types',
-            options: [
-              { value: 'e', label: 'ECTOMORPH', trigger: '1' },
-              { value: 'm', label: 'MESOMORPH', trigger: '2' },
-              { value: 'endo', label: 'ENDOMORPH', trigger: '3' },
-            ]
-          },
-          {
-            id: '1',
-            message: 'The Ectomorph is naturally very thin, has narrow hips and shoulders, very low body fat and very thin arms and legs',
-            trigger: 'intro1',
-          },
-          {
-            id: '2',
-            message: 'The Mesomorph is kind of in between the Ectomorph and the Endomorph. They are able to put on muscle easily and genetically are the ideal body type for bodybuilding. They have very strong legs, broad shoulders and a narrower waist. Generally, they also have very low body fat as well.',
-            trigger: 'intro1',
-          },
-          {
-            id: '3',
-            message: 'The Endomorph is more round and pear-shaped. They tend to store more body fat throughout the entire body, especially in the legs and arms. It’s much harder for the Endomorph to put on muscle and much easier to gain weight.',
-            trigger: 'intro1',
-          },
+            },
+            {
+              id: 'yesno2',
+              options: [
+                { label: 'Diet Plan', trigger: 'yes2' },
+                {  label: 'Workout Plan', trigger: 'no2' },
+              ]
+            },
+            {
+              id: 'yes2',
+              message: `Mesomorphs typically require a higher calorie intake and, according to some nutritionists, 
+              their diets should contain more protein than either of the other body types. Most experts point to a diet of roughly equal parts protein, fats, and carbs.
+            `,
+            trigger:'intro1',
+            },
+            {
+              id: 'no2',
+              message: `When it comes to working out, mesomorphs may have an easier time building muscle, but they typically also gain weight quickly if they stop training. 
+              Mesomorphs would do well to keep a consistent regimen of 30-45 minutes of cardio scheduled three to five times a week.
+            `,
+            trigger:'intro1',
+            },
+            //third body type
+            {
+              id: '3',
+              message: 'The Endomorph is more round and pear-shaped. They tend to store more body fat throughout the entire body, especially in the legs and arms. It’s much harder for the Endomorph to put on muscle and much easier to gain weight.',
+              trigger: 'diet3',
+            },
+            {
+              id: 'diet3',
+              message: 'Do you want to see the diet plan for this particular body-type?',
+              trigger: 'yesno3',
 
+            },
+            {
+              id: 'yesno3',
+              options: [
+                { value: 'yy', label: 'Diet Plan', trigger: 'yes3' },
+                { value: 'nn', label: 'Workout Plan', trigger: 'no3' },
+              ]
+            },
+            {
+              id: 'yes3',
+              message: `mesomorphs typically require a higher calorie intake and, according to some nutritionists, 
+              their diets should contain more protein than either of the other body types. Most experts point to a diet of roughly equal parts protein, fats, and carbs.
+            `,
+              trigger: 'intro1',
+            },
+            {
+              id: 'no3',
+              message: `For exercise, endomorphs should focus primarily on aerobic activities like running, biking, or even dancing.
+               While some exercise experts suggest 30 minutes of moderate aerobic activity 5 days a week, others recommend incorporating activities like high-intensity interval training two or three times a week.
+            `,
+            trigger:'intro1',
+            },
 
-          {
-            id: 'exit-response',
-            message: 'ThankYou! Have a good day!',
-            end: true,
-          },
-         
-        ]}
-        {...config}
-      />
+            //exit button
+            {
+              id: 'exit-response',
+              message: 'ThankYou! Have a good day!',
+              end: true,
+            },
+
+          ]}
+          {...config}
+        />
       </ThemeProvider>
     );
-    
+
   }
 
 }
